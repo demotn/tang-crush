cabcd393a476a6fafb1d0bb9daac9982a0b1ad9f
demotn <87186898+demotn@users.noreply.github.com>
2021-12-27 23:29:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Trang xinh gái ơiiiii!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu anh <33',
-    btnNo: 'Không iu đâu :3',
+    title: 'Phải chăng tớ đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tớ đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'I adore u <3',
+    btnNo: 'Không đâu :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Yêu em nhìu nhìu...công chúa của anh!',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tớ cũng thích cậu <33333333',
+    mess: 'Tớ biết mà 🥰. Love u 😘😘',
+    messDesc: 'Cám ơn cậu vì đã xuất hiện!',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/demotn' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/chinhs58' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
